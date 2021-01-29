456f4384a7ef11b7a0f95fa1b0aba1d3a98af9dc
Amanda D'Argenio <dargenioa14@gmail.com>
2021-01-29 17:50:14 -0500

    added image to homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,31 +1,34 @@
-import {
-  Box,
-  VStack,
-  Heading,
-  Stack,
-  Image,
-  Flex,
-  Center,
-} from "@chakra-ui/react";
+import { Heading, Text, Stack, Center, Image, Box } from "@chakra-ui/react";
 import LandingLayout from "../components/layouts/Landing";
 
 function Home() {
   return (
     <div>
       <LandingLayout />
-      <Heading
-        as="h1"
-        size="4xl"
-        align="center"
+      <Stack
         bgImage="url(https://res.cloudinary.com/dusgzehfe/image/upload/v1611950774/portfolio-background_g9lw6j.jpg)"
         bgPosition="cover"
         bgRepeat="no-repeat"
         h="1000px"
         color="white"
-        p={4}
+        p={17}
       >
-        Amanda D'Argenio
-      </Heading>
+        <Center>
+          <Image
+            src="https://res.cloudinary.com/dusgzehfe/image/upload/v1611960343/PortfolioPicture_nmjj7g.png"
+            boxSize="sm"
+            borderRadius="full"
+          />
+        </Center>
+        <Heading as="h1" size="4xl" align="center">
+          Amanda D'Argenio
+        </Heading>
+        <Heading size="md" p={10} align="center">
+          Full Stack Developer motivated by challenging projects and enjoys the
+          level of focus and problem solving that comes with software
+          development.
+        </Heading>
+      </Stack>
     </div>
   );
 }
